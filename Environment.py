aecb6946547d9cd9cb9706098c1f664552ed8b00
Thiago Grabe <tmgmeireles@hotmlail.com>
2019-10-02 19:36:11 -0300

    sink/tink improvement

--- Environment.py
+++ Environment.py
@@ -18,20 +18,43 @@ class Graph:
 
             # Initialize graph
             self.graph = [[0 for x in range(self.vertex)] for y in range(self.vertex)]
+
             # Edges values
+            direita = []
+            esquerda = []
             for line in self.initial_config:
                 self.graph[line[0]][line[1]] = int(line[2])
+                esquerda.append(line[0])
+                direita.append(line[1])
+            r = self.getUnique(esquerda, direita)
+            self.source, self.sink = r[0][0], r[1][-1]
+            # self.sink = self.getUnique(direita)[0]
 
             self.graph_ = [i[:] for i in self.graph]
 
             self.vertex_list = [i for i in range(self.vertex)]
-            self.sink = [i for i in range(self.vertex)]
-
-            # self.source = random.randrange(self.vertex)
-            self.source = 0
-            self.sink.remove(self.source)
-
+            # self.source = 0
+            # self.sink = [i for i in range(self.vertex)]
+            # self.sink.remove(self.source)
             self.row, self.column = len(self.graph), len(self.graph[0])
+
+    def getUnique(self, source, tink):
+        s = set(dict.fromkeys(source))
+        t = set(dict.fromkeys(tink))
+
+        s_ = list(s.symmetric_difference(t))
+        t_ = list(t.symmetric_difference(s))
+
+
+
+        return s_, t_
+
+
+        # self.unique_list = []
+        # for x in list:
+        #     if x not in self.unique_list:
+        #         self.unique_list.append(x)
+        # return self.unique_list
 
     def reset(self):
         with open(str(self.entry)) as file:
@@ -48,3 +71,11 @@ class Graph:
                 self.graph[line[0]][line[1]] = int(line[2])
 
             self.graph_ = [i[:] for i in self.graph]
+
+    # def source_sink(self):
+    #
+    #     self.source_ = [i[:] for i in self.vertex_list]
+    #     self.source = random.randrange(self.source_)
+    #     self.sink.remove(self.source)
+    #
+    #     self.source_.remove(self.source)
--- MinCut.py
+++ MinCut.py
@@ -1,8 +1,10 @@
+import sys
+import time
+
 from BFS import BFS
 from Environment import Graph
 
-import sys, time
-
+# Global Variables
 results = []
 
 
@@ -13,12 +15,12 @@ def MinCut(graph, source, terminal, search):
     maxFlow = 0
 
     while search.bfs(graph.graph, source, terminal, parent):
-        s = terminal
+        t = terminal
         path = float('inf')
 
-        while s != source:
-            path = min(path, graph.graph[parent[s]][s])
-            s = parent[s]
+        while t != source:
+            path = min(path, graph.graph[parent[t]][t])
+            t = parent[t]
 
         maxFlow += path
         v = terminal
@@ -35,32 +37,49 @@ def MinCut(graph, source, terminal, search):
     for i in range(graph.row):
         for j in range(graph.column):
             if graph.graph[i][j] == 0 and graph.graph_[i][j] > 0:
+                # getNeibours(graph, i, j)
                 i_list.append(i)
                 j_list.append(j)
 
+    # print(i_list, '_', j_list)
 
-    # print("S': ", j_list)
+    # print("S: ", (i_list))
+    # print("S': ", (j_list))
+    # print("S: ", list(dict.fromkeys(i_list)))
+    # print("S': ", list(dict.fromkeys(j_list)))
     # print('Max Flow: ', maxFlow)
-                # print(str(i) + " -", str(j), ' Max Flow: ', maxFlow)
+    # print('------------------')
 
     return i_list, j_list, maxFlow
+
+# def getNeibours(graph, i, j):
+
 
 
 def corteST(results, world):
 
     for answer in results:
-        ans = answer
-        L = [False] * world.vertex
-        for element in answer[0]:
-            if element in world.vertex_list:
-                L[element] = True
-            for element_ in answer[1]:
-                if element in world.vertex_list:
-                    L[element_] = True
+        L = list(dict.fromkeys(answer[0]+answer[1]))
+        a = world.vertex_list
+        b = answer[2]
+        res = all(elem in L for elem in world.vertex_list)
+        if res:
+            return answer
+        else:
+            continue
 
-        if all(element for element in L):
-            break
-    return ans
+        # ans = answer
+        # L = [False] * world.vertex
+    #     for element in answer[0]:
+    #         if element in world.vertex_list:
+    #             L[element] = True
+    #         for element_ in answer[1]:
+    #             if element in world.vertex_list:
+    #                 L[element_] = True
+    #
+    #     if all(element for element in L):
+    #         break
+    # return ans
 
 
 def main():
@@ -69,26 +88,46 @@ def main():
     try:
         file = sys.argv[1]
     except:
-        file = str('entrada_1')
+        file = str('11.in')
     world = Graph(str(file))
     search = BFS()
     source = world.source
     terminal = world.sink
-    for sink in terminal:
-        # print('Source: ', source, 'Sink: ', sink)
-        # print('----------------------------------')
+    # world.source_sink()
+    # for source in world.source_:
+    # for sink in terminal:
+    print('Source: ', source, 'Sink: ', world.sink)
+    print('----------------------------------')
 
-        # source = 0
-        # terminal = 1
-        answer = MinCut(world, source, sink, search)
-        results.append(answer)
-        world.reset()
-    final_result = corteST(results, world)
-    print('Qty Vertices: ', len(list(dict.fromkeys(final_result[0]))))
-    print('Conjunto S:', list(dict.fromkeys(final_result[0])))
-    print('Conjunto S":', list(dict.fromkeys(final_result[1])))
-    print('Max-Flow: ', final_result[-1])
+    # source = 0
+    # terminal = 1
+    answer = MinCut(world, source, world.sink, search)
+
+    # results.append(answer)
+    # world.reset()
+    # world.source_sink()
+    # final_result = corteST(results, world)
+    # if final_result is None:
+    #     for weight in results:
+    #         idx, val = 0, float('inf')
+    #         if weight[2] < val:
+    #             final_result = weight
+    #             val = weight[2]
+    #         idx += 1
+    # print(results)
+    # S = set(list(dict.fromkeys(final_result[0])))
+    # S_ = set(list(dict.fromkeys(final_result[1])))
+    S = answer[0]
+    S_ = answer[1]
+    # t = S - S_
+
+    # print('Qty Vertices: ', len(S))
+    # print('Conjunto S:', S)
+    print('Qty Vertices: ', len(list(dict.fromkeys(S_))))
+    print('Conjunto S:', list(dict.fromkeys(S_)))
+    print('Sum cut weights: ', answer[-1])
     print('Time: ', time.time() - init)
+
 
 if __name__ == '__main__':
     main()
